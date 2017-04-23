9e92493b46c90e9335ed41b733cf92bf234e9bbc
Lucas <lucashen@gmail.com>
2017-04-23 17:43:14 -0300

    add relatorios frontend

--- public/js/controllers/relatorios.controller.js
+++ public/js/controllers/relatorios.controller.js
@@ -5,10 +5,21 @@
         .module('app')
         .controller('RelatoriosController', RelatoriosController);
 
-    RelatoriosController.$inject = ['$scope'];
+    RelatoriosController.$inject = ['$scope', 'DataService'];
 
-    function RelatoriosController($scope) {
+    function RelatoriosController($scope, DataService) {
 
+        DataService.getProdutos().then(function(response) {
+            $scope.produtos = response;
+        }, function (error) {
+            toastr.error('Erro ao buscar os produtos', 'Produto', {timeOut: 3000});
+        });
+
+        DataService.getFornecedores().then(function(response) {
+            $scope.fornecedores = response;
+        }, function (error) {
+            toastr.error('Erro ao buscar os fornecedores', 'Fornecedor', {timeOut: 3000});
+        });
     }
 
 })();
