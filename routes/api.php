e84cbebd50d7543f2e19a5e08d85f088b248385d
Lucas <lucashen@gmail.com>
2017-05-20 12:32:23 -0300

    ajusta sistema para o cartorio

--- routes/api.php
+++ routes/api.php
@@ -18,46 +18,6 @@ $api = $app->make(Dingo\Api\Routing\Router::class);
 
 $api->version('v1', function ($api) {
 
-    $api->get('/produtos', [
-        'uses' => 'App\Http\Controllers\ProdutoController@produtos',
-        'as' => 'api.produtos'
-    ]);
-
-    $api->post('/produto', [
-        'uses' => 'App\Http\Controllers\ProdutoController@addProduto',
-        'as' => 'api.add.produto'
-    ]);
-
-    $api->delete('/produto', [
-        'uses' => 'App\Http\Controllers\ProdutoController@removeProduto',
-        'as' => 'api.remove.produto'
-    ]);
-
-    $api->put('/produto', [
-        'uses' => 'App\Http\Controllers\ProdutoController@atualizarProduto',
-        'as' => 'api.atualizar.produto'
-    ]);
-
-    $api->get('/fornecedores', [
-        'uses' => 'App\Http\Controllers\FornecedorController@fornecedores',
-        'as' => 'api.fornecedores'
-    ]);
-
-    $api->post('/fornecedor', [
-        'uses' => 'App\Http\Controllers\FornecedorController@addFornecedor',
-        'as' => 'api.add.fornecedor'
-    ]);
-
-    $api->delete('/fornecedor', [
-        'uses' => 'App\Http\Controllers\FornecedorController@removeFornecedor',
-        'as' => 'api.remove.fornecedor'
-    ]);
-
-    $api->put('/fornecedor', [
-        'uses' => 'App\Http\Controllers\FornecedorController@atualizarFornecedor',
-        'as' => 'api.atualizar.fornecedor'
-    ]);
-
     $api->get('/estados', [
         'uses' => 'App\Http\Controllers\EstadoCidadeController@estados',
         'as' => 'api.estados'
@@ -68,37 +28,6 @@ $api->version('v1', function ($api) {
         'as' => 'api.cidades'
     ]);
 
-    $api->get('/notas-entrada', [
-        'uses' => 'App\Http\Controllers\NotaEntradaController@notas',
-        'as' => 'api.notas.entrada'
-    ]);
-
-    $api->post('/nota-entrada', [
-        'uses' => 'App\Http\Controllers\NotaEntradaController@addNotaEntrada',
-        'as' => 'api.add.nota.entrada'
-    ]);
-
-    $api->delete('/nota-entrada', [
-        'uses' => 'App\Http\Controllers\NotaEntradaController@removeNotaEntrada',
-        'as' => 'api.remove.nota.entrada'
-    ]);
-
-    $api->put('/nota-entrada', [
-        'uses' => 'App\Http\Controllers\NotaEntradaController@atualizarNotaEntrada',
-        'as' => 'api.atualizar.nota.entrada'
-    ]);
-
-    // relatorio
-    $api->post('/relatorio', [
-        'uses' => 'App\Http\Controllers\RelatorioController@relatorio',
-        'as' => 'api.relatorio'
-    ]);
-
-    $api->get('/pesquisa-nfe', [
-        'uses' => 'App\Http\Controllers\NotaEntradaController@pesquisaNfe',
-        'as' => 'api.pesquisa.nfe'
-    ]);
-
     // dashboard
     $api->get('/dashboard', [
         'uses' => 'App\Http\Controllers\Controller@dashboard',
@@ -107,7 +36,6 @@ $api->version('v1', function ($api) {
 
 
     // authentication
-
     $api->post('/auth/login', [
         'as' => 'api.auth.login',
         'uses' => 'App\Http\Controllers\Auth\AuthController@postLogin',
